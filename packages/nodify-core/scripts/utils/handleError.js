20e9ececdba919e4f8e09d1a540c12a3125c26ab
Shriyans <shriyansbhatnagar@gmail.com>
2017-10-31 18:41:45 +0900

    fix(error): shorter error messages
    shortend error messages using relative path
    
    no breaking changes

--- packages/nodify-core/scripts/utils/handleError.js
+++ packages/nodify-core/scripts/utils/handleError.js
@@ -4,21 +4,32 @@ const chalk = require('chalk');
 const relativeId = require('./relativeId');
 const stderr = require('./stderr.js');
 
+const replaceRelativePath = str => {
+  const cwd = process.cwd();
+  const re = new RegExp(cwd, 'g');
+  return str.replace(re, '.');
+};
+
 function handleError(err) {
   let description = err.message || err;
   if (err.name) {
     description = `${err.name}: ${description}`;
   }
   const message =
-    (err.plugin ? `(${err.plugin} plugin) \n${description}` : description) || err;
-  stderr(chalk.bold.red(`[!] ${chalk.bold(message)}`));
+    (err.plugin ? `(${err.plugin} plugin) \n${description}` : description) ||
+    err;
+  stderr(chalk.bold.red(`[!] ${chalk.bold(replaceRelativePath(message))}`));
 
   if (err.url) {
-    stderr(chalk.cyan(err.url));
+    stderr(chalk.cyan(replaceRelativePath(err.url)));
   }
 
   if (err.loc) {
-    stderr(`${err.loc.file || err.id} (${err.loc.line}:${err.loc.column})`);
+    stderr(
+      replaceRelativePath(
+        `${err.loc.file || err.id} (${err.loc.line}:${err.loc.column})`
+      )
+    );
   } else if (err.id) {
     stderr(relativeId(err.id));
   }
@@ -30,9 +41,9 @@ function handleError(err) {
   }
 
   if (err.frame) {
-    stderr(chalk.gray(err.frame));
+    stderr(chalk.gray(replaceRelativePath(err.frame)));
   } else if (err.stack) {
-    stderr(chalk.gray(err.stack));
+    stderr(chalk.gray(replaceRelativePath(err.stack)));
   }
 }
 
