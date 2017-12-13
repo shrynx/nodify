9066a627d95c5121fbcb9dbbf0c960a74636b898
Shriyans <shriyansbhatnagar@gmail.com>
2017-12-13 22:04:08 +0900

    chore(examples): fixed serverless example
    fixed serverless example for customizing rollup config
    
    none

--- examples/serverless/nodify.config.js
+++ examples/serverless/nodify.config.js
@@ -7,10 +7,7 @@ module.exports = {
       // using resolve as lamdas needs node_modules bundled up
       // and babel-minify is optional but just to reduce size as serverless has a size limit.
       config.plugins.push(resolve(), minify());
-      // optional, deleting the banner which adds source-map support
-      delete config.output.banner;
     }
-
     return config;
   },
 };
