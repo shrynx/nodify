3fa012cbe74f30609e3f9c769839929705885839
Shriyans Bhatnagar <shriyansbhatnagar@gmail.com>
2018-03-08 23:25:41 +0900

    chore(examples): updated examples
    updated all the examples to use the latest version of nodify-core

--- examples/serverless/nodify.config.js
+++ examples/serverless/nodify.config.js
@@ -1,12 +1,12 @@
 const resolve = require('rollup-plugin-node-resolve');
-const minify = require('rollup-plugin-babel-minify');
+const uglify = require('rollup-plugin-uglify');
 
 module.exports = {
   rollup: (config, env) => {
     if (env === 'production') {
       // using resolve as lamdas needs node_modules bundled up
       // and babel-minify is optional but just to reduce size as serverless has a size limit.
-      config.plugins.push(resolve(), minify());
+      config.plugins.push(resolve(), uglify());
     }
     return config;
   },
